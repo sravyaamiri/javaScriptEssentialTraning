bad1f7ed824afaaaa52bb14a74b0f26787c4f809
sravyaamiri <33321296+sravyaamiri@users.noreply.github.com>
2021-04-18 11:06:33 +0200

    Create script.js

---
+++ automatedResponsiveImages/script.js
@@ -0,0 +1,33 @@
+const IMAGES = document.queryselectorAll("img");
+const SIZES = {
+    showcase: "100vw",
+    reason: "(max-width: 799px) 100vw, 372px",
+    feature: "(max-width: 799px) 100vw, 558px",
+    story: "(max-width: 799px) 100vw, 670px",
+};
+
+function makeSrcset(imgScr){
+  let markup = [];
+  let width = 400;
+  
+  for(let i=0; i<5; i++){
+    markup[i] = imgSrc +"-"+ width + ".jpg" + width + "w":
+    width+=400;
+  }
+  return.markup.join();
+}
+
+for(let i=0;i<IMAGES.length; i++){
+  
+  let imgSrc=IMAGES[i].getAttribute("src");
+  imgSrc= imgScr.slice(0,-8);
+  let srcset=makeScrset(imgSrc);
+  IMAGES[i].setAttribute("scrset",srcset);
+  
+  let type= IMAGES[i].getAttribute("data-type");
+  let sizes =SIZES[type];
+  IMAGES[i].setAttribute("sizes", sizes);
+ 
+}
+    
+    
